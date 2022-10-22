d78a2df06ac96d1c5410c64943ad35901e21a9f4
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-10-22 15:04:32 +0530

    Completed the station find view

---
+++ app/src/main/java/com/example/fuelex/StationView.java
@@ -0,0 +1,14 @@
+package com.example.fuelex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StationView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fuel_station_find_view);
+    }
+}
